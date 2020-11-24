7aad6b577db2dfa20cc0f981fd48d8cd67163a79
Maria Jose <mariacijim@gmail.com>
2020-11-24 23:56:32 +0000

    code to remove requested element, compare the asnwer to given asnwer and then print assertion message

---
+++ without.js
@@ -0,0 +1,47 @@
+const without = function (A,B) {
+  let result = [];
+  let status = false;
+  for (let itemA of A) { //picks first element from given words
+    for (let itemB of B) { //picks elements one by one from removing list
+      if (itemA === itemB) {
+        status = true;
+      } else {
+        status = false;
+      }
+    }
+    if (!status) {
+      result.push(itemA);
+      status = true;
+    }
+  }
+
+  return result;
+}
+
+const eqArrays = function (a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+const assertArraysEqual = function (actual, remove, answer) {
+  let ourAnswer = without(actual,remove);
+  if(eqArrays(ourAnswer,answer)) {
+    console.log(`✅✅✅Assertion Passed: ${ourAnswer} === ${answer}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${ourAnswer} !== ${answer}`);
+  }
+}
+
+
+
+assertArraysEqual(["hello", "world", "lighthouse","Maria"],["lighthouse"], ["hello", "world"]);
+assertArraysEqual(["1", "2", "3"], [1, 2, "3"], ["1", "2"]);
