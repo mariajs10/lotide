73347afe34cc0f6c14e3d48f6f425649178ec382
Maria Jose <mariacijim@gmail.com>
2020-11-29 00:09:07 +0000

    using tailTest.js tail.js

--- tail.js
+++ tail.js
@@ -1,14 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return true;
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    return false;
-  }
-};
-
+//const assertEqual = require('./assertEqual');
 const tail = function (array) {
   let newArray = []
   for (let i = 1; i < array.length; i++) {
@@ -17,10 +8,4 @@ const tail = function (array) {
   return newArray;
 }
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
