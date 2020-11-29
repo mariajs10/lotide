4ba2248ad1c08712c97354c3e7add485380ff991
Maria Jose <mariacijim@gmail.com>
2020-11-29 00:17:17 +0000

    test file for eqArrays.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 1, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
